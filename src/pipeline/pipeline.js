1740ec0ff77fc69e0bf4efd1d9ec9ab50df8d3d1
Corey Ferguson <corey.t.ferguson@gmail.com>
2017-10-07 14:59:48 -0700

    if operation.hasCachedAnswers then dont inquire

--- src/pipeline/pipeline.js
+++ src/pipeline/pipeline.js
@@ -13,15 +13,24 @@ class Pipeline {
 
   run() {
     return bluebird.mapSeries(ioc.operations, operation => {
-      const label = operation.getLabel();
-      log.info(`Processing: ${label}`);
-      const cachedAnswers = ioc.cache.fetch(label);
+      log.info(`Processing: ${operation.getLabel()}`);
+      return this._inquire(operation);
+    });
+  }
+
+  _inquire(operation) {
+    const label = operation.getLabel();
+    const cachedAnswers = ioc.cache.fetch(label);
+    if (!cachedAnswers) {
+      log.debug(`Inquiring into '${label}' operation`);
       return operation.inquire(cachedAnswers).then(questions => {
         return inquirer.prompt(questions).then(answers => {
           ioc.cache.put(label, answers);
         });
       });
-    });
+    } else {
+      log.debug(`Operation '${label}' already has cached answers`);
+    }
   }
 
 }

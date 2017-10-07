3f410ed804b7e9c17c43c9154439271009f80563
Corey Ferguson <corey.t.ferguson@gmail.com>
2017-10-07 07:29:30 -0700

    chai-as-expected

--- test/spec/001-test-environment.js
+++ test/spec/001-test-environment.js
@@ -3,13 +3,18 @@ const { expect } = require('../support/testUtils');
 
 describe('test environment', () => {
 
-  it('`describe` and `it` both defined', () => {
+  it('`describe` and `it` from mocha', () => {
     // if we've come this far without error, success!
   });
 
-  it('`expect` from chai is available', () => {
+  it('`expect` from chai', () => {
     expect(true).to.be.true;
     expect(false).to.not.be.true;
+  });
+
+  it('chai-as-expected', () => {
+    return expect(new Promise(resolve => setTimeout(resolve, 20)))
+      .to.eventually.be.fulfilled;
   });
 
 });
